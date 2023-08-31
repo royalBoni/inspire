91a2c0d6b1a76c4aff3a6034614cc342f9b0056f
royalBoni <bonifaceemmanuel13@gmail.com>
2023-08-31 12:30:00 +0100

    adding the server link

--- src/reducxSlices/api.js
+++ src/reducxSlices/api.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'api',  //optional
     /* baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3030'}), */ 
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000'}), //https://food-api-x4wn.onrender.com  http://localhost:5000
+    baseQuery: fetchBaseQuery({baseUrl: 'https://inspire-api.onrender.com'}), //  http://localhost:5000
     tagTypes: ['Post'],
     endpoints: builder =>({})
 })
